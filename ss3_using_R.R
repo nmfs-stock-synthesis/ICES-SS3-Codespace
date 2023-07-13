d3979b2af219b3b42527d69cf6e5f14937da1383
Elizabeth Gugliotti <elizabeth.gugliotti@noaa.gov>
2023-07-13 17:24:37 +0000

    add R file to work run model

---
+++ ss3_using_R.R
@@ -0,0 +1,17 @@
+library(r4ss)
+
+# Get info about your working directory
+getwd()
+list.files(getwd(), full.names = TRUE)
+
+# This downloads the SS3 exe to your working directory
+get_ss3_exe()
+
+# This downloads the SS3 to your working directory, provided that directory is the
+# cloned repository from GitHub
+get_ss3_exe(dir = file.path(getwd(), "herring-just-2019-as-equilibrium"))
+
+# Run the model, despite already having a report.sso file in there (that is why we
+# set skipfinsihed = FALSE)
+r4ss::run(dir = file.path(getwd(), "herring-just_2019-as-equilibirum"), exe = "ss3", 
+          skipfinished = FALSE)
